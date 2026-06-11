8bfc324e40887fa6f370ed7272de6a5abe2eadb6
Kaelyx <scott-donaldson@outlook.com>
2026-06-11 13:57:53 +0100

    Update font styling options to use new font faces

--- kaelyx/vite.config.ts
+++ kaelyx/vite.config.ts
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    open: true,
+    host: true
+  },
   plugins: [
     vue(),
     vueDevTools(),
